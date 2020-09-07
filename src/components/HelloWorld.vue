271ebd35ad9caddcc0fb429c8a1f0e6aafb87e60
jonhetrick <jhetrick@seattleu.edu>
2020-09-06 23:36:18 -0700

    Added Cards for Data Response, Images of moon phases

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,20 +10,71 @@
                 label="Your Current Address"
                 required
               />
-              <v-btn text @click="locatorButtonPressed">Current</v-btn>
-              <v-btn text @click="moonPhasePuller">Search for lunar</v-btn>
+              <v-btn
+                color="warning"
+                rounded
+                dark
+                text
+                @click="locatorButtonPressed"
+                >Current</v-btn
+              >
+              <v-btn color="warning" rounded dark text @click="moonPhasePuller"
+                >Search for lunar</v-btn
+              >
             </v-row>
           </v-container>
-          <v-container>{{ DS_response }}</v-container>
-          <div id="app">
-            <h1>Info:</h1>
-            <div v-for="i in DS_response" :key="i" class="i">
-              {{ moonphase }}
-            </div>
-          </div>
         </v-form>
       </v-col>
     </v-row>
+    <v-card v-if="DS_response !== null" class="mx-auto" max-width="400">
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline"
+            >Daily Intel Drop</v-list-item-title
+          >
+          <v-list-item-subtitle>{{ summary }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-card-text>
+        <v-row align="center">
+          <v-col class="display-3" cols="6">{{ moonPhase * 100 + "%" }}</v-col>
+          <v-col cols="6">
+            <v-img src="../assets/cards/half.png" alt="Sunny image"></v-img>
+          </v-col>
+        </v-row>
+      </v-card-text>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-send</v-icon>
+        </v-list-item-icon>
+        <v-list-item-subtitle>{{ windSpeed }} km/h</v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-cloud</v-icon>
+        </v-list-item-icon>
+        <v-list-item-subtitle>{{
+          cloudCover * 100 + "%"
+        }}</v-list-item-subtitle>
+      </v-list-item>
+
+      <!-- <v-list class="transparent">
+        <v-list-item v-for="item in forecast" :key="item.day">
+          <v-list-item-title>{{ item.day }}</v-list-item-title>
+
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-subtitle class="text-right">{{
+            item.temp
+          }}</v-list-item-subtitle>
+        </v-list-item>
+      </v-list> -->
+    </v-card>
   </v-container>
 </template>
 
@@ -35,11 +86,17 @@ export default {
 
   data() {
     return {
+      message: "",
       address: "",
       lat: 0,
       lng: 0,
       DS_response: null,
-      loading: true,
+      summary: null,
+      moonPhase: null,
+      visibility: null,
+      cloudCover: null,
+      sunsetTime: null,
+      windSpeed: null,
       errored: false,
       API_KEY: "AIzaSyCptaE0KIUwUGLJjqYNfPfWGnGN6sZN_EM",
       DS_Key: "9091f71b89d453129c7e893656767cfa"
@@ -90,12 +147,40 @@ export default {
         .get(
           `https://api.darksky.net/forecast/${this.DS_Key}/${this.lat},${this.lng}`
         )
-        .then(response => (this.DS_response = response.data.daily.data[0]))
+        .then(
+          response => (
+            (this.DS_response = response.data.daily.data[0]),
+            (this.summary = response.data.daily.data[0].summary),
+            (this.moonPhase = response.data.daily.data[0].moonPhase),
+            (this.visibility = response.data.daily.data[0].visibility),
+            (this.sunsetTime = response.data.daily.data[0].sunsetTime),
+            (this.cloudCover = response.data.daily.data[0].cloudCover),
+            (this.windSpeed = response.data.daily.data[0].windSpeed)
+          )
+        )
         .catch(error => {
           console.log(error);
           this.errored = true;
         })
         .finally(() => (this.loading = false));
+    },
+    brain(phase) {
+      if (phase <= 0.25) {
+        return "Waxing or Waning Cresent";
+      } else if (phase > 0.25 && this.moonPhase < 0.75) {
+        return "First or Last Quarter";
+      } else if (phase >= 0.75) {
+        return "Waxing or Waning Gibbous";
+      } else if (phase === 0.0) {
+        return "new Moon";
+      } else {
+        return "Full moon";
+      }
+    },
+    filters: {
+      percent: function(val) {
+        return val * 100 + "%";
+      }
     }
   }
 };
